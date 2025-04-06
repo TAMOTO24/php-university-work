8819f7083aa59a51f1a16271a8461215edc8d6fe
Keshee <sanicyll@gmail.com>
2025-04-06 20:26:34 +0300

    LAB 2: Implement test CRUD methods in TestController's of Laravel and Symphony.

--- laravel/example-app/app/Http/Controllers/testController.php
+++ laravel/example-app/app/Http/Controllers/testController.php
@@ -11,4 +11,38 @@ class testController extends Controller
     {
         return response()->json(['message' => 'Test method works!']);
     }
+    // Симулюємо створення нового повідомлення
+    public function create()
+    {
+        // Симулюємо створення нового повідомлення
+        $message = 'This is a new message';
+
+        // Виводимо повідомлення
+        return response()->json(['message' => 'Message created with text: ' . $message]);
+    }
+
+    // Симулюємо читання повідомлення за ID
+    public function read($id)
+    {
+        // Симулюємо пошук повідомлення за ID
+        $message = "Message with ID $id";
+
+        return response()->json(['message' => $message]);
+    }
+
+    // Симулюємо оновлення повідомлення
+    public function update($id)
+    {
+        // Симулюємо оновлення повідомлення
+        $updatedMessage = "Updated message for ID $id";
+
+        return response()->json(['message' => 'Message updated to: ' . $updatedMessage]);
+    }
+
+    // Симулюємо видалення повідомлення
+    public function delete($id)
+    {
+        // Симулюємо видалення повідомлення
+        return response()->json(['message' => 'Message with ID ' . $id . ' has been deleted!']);
+    }
 }
--- laravel/example-app/routes/web.php
+++ laravel/example-app/routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', [testController::class, 'test']);
+Route::get('/test/create', [TestController::class, 'create']);
+Route::get('/test/{id}', [TestController::class, 'read']);
+Route::put('/test/update/{id}', [TestController::class, 'update']);
+Route::delete('/test/delete/{id}', [TestController::class, 'delete']);
